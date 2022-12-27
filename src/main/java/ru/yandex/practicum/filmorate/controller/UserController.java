c376add5a4eb61cf96e3eb0284c51e1dc4efbd15
Ermachenkov <ermac.e@yandex.ru>
2022-12-27 23:21:40 +0300

    fix: fix empty name

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -70,7 +70,8 @@ public class UserController {
                 throw new ValidationException("Логин не может быть пустым и содержать пробелы", method);
             } else if(user.getBirthday().isAfter(LocalDate.now())){
                 throw new ValidationException("Дата рождения не может быть в будущем.", method);
-            } else if(user.getName().isEmpty()){
+            } else if(user.getName() == null){
+                System.out.println("test");
                 user.setName(user.getLogin());
                 return true;
             }
