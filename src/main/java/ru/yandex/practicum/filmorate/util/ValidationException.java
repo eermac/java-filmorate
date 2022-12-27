c36fb74a207bec61c2de560dc501311f05b637bb
Ermachenkov <ermac.e@yandex.ru>
2022-12-27 21:45:41 +0300

    fix: fix exception

--- src/main/java/ru/yandex/practicum/filmorate/util/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/ValidationException.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-public class ValidationException extends IOException {
+public class ValidationException extends RuntimeException {
     private final static Logger log = LoggerFactory.getLogger(ValidationException.class);
     public ValidationException(final String message){
-        super(message);
         log.error(message);
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
     }
 }
+
+
